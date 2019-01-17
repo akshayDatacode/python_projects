8228bcf4431c429cc6ccc771ef07ea9c230159fd
akshayDatacode <akshay.webllisto@gmail.com>
2019-01-17 17:52:28 +0530

    Login Python code is Ready and succesfull with database

--- login.py
+++ login.py
@@ -1,7 +1,6 @@
 import pymysql
 
-a= input("Enter 1 for Register")
-
+a= input("Enter 1 for Register and 2 for Login")
 if a=="1":
 	name=input("Enter Name:-")
 	cnt=input("Enter Phone no.:-")
@@ -24,8 +23,8 @@ if a=="1":
 
 
 if a=="2":
-	userid=input("create User ID:-")
-	password=input("Creat password")
+	userid=input(" User ID:--")
+	password=input("Password:--")
     
 	db=pymysql.connect("localhost","root","Abc123@#$","akshay")
 	cursor = db.cursor()
@@ -35,14 +34,16 @@ if a=="2":
    		cursor.execute(sql,(userid))
    		db.commit()
    		result = cursor.fetchone()
-   		print(result)
-	  #   if result==password:
-			# print("Heloo succeess")
-    
-
-
+   		result = result[0]
 	except:
-   		db.rollback()
+		db.rollback()
+		print("UserID not found   TRY AGAIN")
+   		
   
+
+if result==password:
+	print("Heloo succeess")
+else:
+	print("Password is Wrrong")	
 # COMMENT GIVE	
 #some comment
